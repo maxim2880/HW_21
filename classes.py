117775a296e4ff89fc9cc0aadd369df0f8ac94c5
admin <misuramaxim@gmail.com>
2022-08-01 10:51:44 +0500

    Initial commit

---
+++ classes.py
@@ -0,0 +1,143 @@
+from abc import ABC, abstractmethod
+
+
+class Storage(ABC):
+    @abstractmethod
+    def add(self, name, count):
+        pass
+
+    @abstractmethod
+    def remove(self, count):
+        pass
+
+    @abstractmethod
+    def get_free_space(self):
+        pass
+
+    @abstractmethod
+    def get_items(self):
+        pass
+
+    @abstractmethod
+    def get_unique_items_count(self, count):
+        pass
+
+
+class Store(Storage):
+
+    def __init__(self, items: dict, capacity=100):
+        self.__items = items
+        self.__capacity = capacity
+
+    @property
+    def capacity(self):
+        return self.__capacity
+     
+    @capacity.setter
+    def capacity(self, count):
+        self.__capacity = count
+
+    def add(self, name, count):
+        if name in self.__items.keys():
+            if self.get_free_space() >= count:
+                print("Товар добавлен")
+                self.__items[name] += count
+                return True
+            else:
+                if isinstance(self, Shop):
+                    print("Недостаточно места в магазине")
+                else:
+                    print("Недостаточно места на складе")
+                return False
+        else:
+            if self.get_free_space() >= count:
+                print("Товар добавлен")
+                self.__items[name] = count
+                return True
+            else:
+                if isinstance(self, Shop):
+                    print("Недостаточно места в магазине")
+                else:
+                    print("Недостаточно места на складе")
+                return False
+
+    def remove(self, name, count):
+        if self.__items[name] >= count:
+            print("Нужное количество есть на складе")
+            self.__items[name] -= count
+            return True
+        else:
+            print("Недостаточно места на складе")
+            return False
+
+    def get_free_space(self):
+        current_space = 0
+        for value in self.__items.values():
+            current_space += value
+        return self.__capacity - current_space
+
+    @property
+    def get_items(self):
+        return self.__items
+
+    def get_unique_items_count(self):
+        return len(self.__items.keys())
+
+    def __str__(self):
+        st = "\n"
+        for key, value in self.__items.items():
+            st += f"{key}: {value}\n"
+        return st
+
+
+class Shop(Store):
+    def __init__(self, items, capacity=20):
+        super().__init__(items, capacity)
+
+    def add(self, name, count):
+        if self.get_unique_items_count() >= 5:
+            print ("Слишком много уникальных товаров")
+            return False
+        else:
+            super().add(name, count)
+
+
+class Request:
+    def __init__(self, request_str):
+        req_list = request_str.split()
+        action = req_list[0]
+        self.__count = int(req_list[1])
+        self.__item = req_list[2]
+        if action == "Доставить":
+            self.__from = req_list[4]
+            self.__to = req_list[6]
+        elif action == "Забрать":
+            self.__from = req_list[4]
+            self.__to = None
+        elif action == "Привезти":
+            self.__to = req_list[4]
+            self.__from = None
+
+    def move(self):
+        if self.__to and self.__from:
+            if eval(self.__to).add(self.__item, self.__count):
+                eval(self.__from).remove(self.__item, self.__count)
+        elif self.__to:
+            eval(self.__to).add(self.__item, self.__count)
+        elif self.__from:
+            eval(self.__from).remove(self.__item, self.__count)
+
+
+storage_1 = Shop(items={"Телефон":10, "Компьютер":20, "Телевизор":20}, capacity=100)
+storage_2 = Shop(items={"Телефон":10, "Компьютер":20, "Приставка":20}, capacity=100)
+shop_1 = Shop(items={"Телефон":3, "Компьютер":3, "Приставка":3})
+
+
+# storage_1=Shop(items = {"Телефон":10, "Компьютер":20}, capacity=100)
+#
+# storage_1.add("Планшет", 5)
+# storage_1.remove("Телефон", 2)
+# print(storage_1.get_free_space())
+# print(storage_1.get_items())
+# print(storage_1.get_unique_items_count())
+# print(storage_1)
---
+++ main.py
@@ -0,0 +1,18 @@
+from classes import storage_1, storage_2, shop_1, Request
+
+while True:
+    print("Текущие площади:")
+    print(f"storage_1: {storage_1}")
+    print(f"storage_2: {storage_2}")
+    print(f"shop_1: {shop_1}")
+
+    user_text = input("Введите команду\n")
+
+    if user_text == "стоп":
+        break
+    else:
+        try:
+            req = Request(user_text)
+            req.move()
+        except Exception as e:
+            print(f"Произошла ошибка {e}. Попробуйте еще раз")
